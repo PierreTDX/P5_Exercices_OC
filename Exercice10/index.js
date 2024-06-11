b9aecae544d8b95ddf07c0253898a7c80d0c2ecc
PierreTDX <pierre.tondeux@gmail.com>
2024-06-11 10:41:31 +0200

    exercices 4 et 7 ne passent pas le test

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,9 @@
-//votre code ici
+function printNumbers(n) {
+    const numbers = [];
+    for (let i = 1; i <= n; i++) {
+        numbers.push(i);
+    }
+    return numbers.join(' ');
+}
 
-export default printNumbers
+export default printNumbers;
--- Exercice8/script.js
+++ Exercice8/script.js
@@ -1,3 +1,12 @@
-//Votre code ici
+function add(a, b) {
+    let num1 = parseFloat(a);
+    let num2 = parseFloat(b);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error("les 2 variables ne sont pas au format nombre");
+    }
+
+    return num1 + num2;
+}
 
  export default add;
